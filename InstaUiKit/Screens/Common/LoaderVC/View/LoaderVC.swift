ecf9c81c6bf41a45e602e05c8210bd98877fe0db
Darshan <darshanm.itpath@gmail.com>
2023-10-27 15:05:25 +0530

    add Loader

---
+++ InstaUiKit/Screens/Common/LoaderVC/View/LoaderVC.swift
@@ -0,0 +1,15 @@
+//
+//  LoaderVC.swift
+//  InstaUiKit
+//
+//  Created by IPS-161 on 27/10/23.
+//
+
+import UIKit
+
+class LoaderVC: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
