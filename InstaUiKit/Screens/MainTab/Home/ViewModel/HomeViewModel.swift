ae1fdf46fe2e90517344ab3f0b9ad62a8de55552
Darshan <darshanm.itpath@gmail.com>
2023-10-31 14:31:25 +0530

    final changes relatedd stories

--- InstaUiKit/Screens/MainTab/Home/ViewModel/HomeViewModel.swift
+++ InstaUiKit/Screens/MainTab/Home/ViewModel/HomeViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 class HomeVCViewModel {
     static let shared = HomeVCViewModel()
     var userArray = [[String: String]]()
-    
+
     func fetchUniqueUsers(completionHandler: @escaping (Bool) -> Void) {
         PostViewModel.shared.fetchAllPosts { result in
             switch result {
@@ -18,16 +18,23 @@ class HomeVCViewModel {
                 // Handle the images
                 print("Fetched images: \(images)")
                 DispatchQueue.main.async {
-                    for post in images {
-                        let uid = post.uid
-                        let name = post.name
-                        
-                        // Check if the uid is not already in userArray
-                        if !self.userArray.contains(where: { $0[uid] != nil }) {
-                            self.userArray.append([uid: name])
+                    Data.shared.getData(key: "CurrentUserId") { (result: Result<String?, Error>) in
+                        switch result {
+                        case .success(let currentUid):
+                            for post in images {
+                                let uid = post.uid
+                                let name = post.name
+                                // Check if the uid is not already in userArray and is not the same as currentUid
+                                if uid != currentUid, !self.userArray.contains(where: { $0[uid] != nil }) {
+                                    self.userArray.append([uid: name])
+                                }
+                            }
+                            completionHandler(true)
+                        case .failure(let failure):
+                            print(failure)
+                            completionHandler(false)
                         }
                     }
-                    completionHandler(true)
                 }
             case .failure(let error):
                 // Handle the error
