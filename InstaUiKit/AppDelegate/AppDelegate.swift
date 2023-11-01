18f11fb90fa8639a74750a6549ab833542066c2e
Darshan <darshanm.itpath@gmail.com>
2023-11-01 10:14:10 +0530

    new

--- InstaUiKit/AppDelegate/AppDelegate.swift
+++ InstaUiKit/AppDelegate/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return orientationLock
     }
     
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
