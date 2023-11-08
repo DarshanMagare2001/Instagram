897a38622d4dcb52082e6301514da26919fc1fee
Darshan <darshanm.itpath@gmail.com>
2023-11-08 18:00:28 +0530

    new changes

---
+++ InstaUiKit/Common/Feed/View/CommentCell/CommentCell.swift
@@ -0,0 +1,19 @@
+//
+//  CommentCell.swift
+//  InstaUiKit
+//
+//  Created by IPS-161 on 08/11/23.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
--- InstaUiKit/Common/Feed/View/CommentsVC.swift
+++ InstaUiKit/Common/Feed/View/CommentsVC.swift
@@ -8,14 +8,25 @@
 import UIKit
 
 class CommentsVC: UIViewController {
-
+    @IBOutlet weak var tableViewOutlet: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-
+    
     @IBAction func backBtnPressed(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
     
+}
+
+extension CommentsVC : UITableViewDelegate , UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell", for: indexPath) as! CommentCell
+        return cell
+    }
 }
