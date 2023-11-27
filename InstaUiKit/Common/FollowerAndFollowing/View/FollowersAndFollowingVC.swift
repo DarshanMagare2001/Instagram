14de28040a9c5d5c953088faab5dcfb9c1a2182b
Darshan <darshanm.itpath@gmail.com>
2023-11-27 12:36:17 +0530

    few new changes

---
+++ InstaUiKit/Common/FollowerAndFollowing/View/FollowersAndFollowingVC.swift
@@ -0,0 +1,22 @@
+//
+//  FollowersAndFollowingVC.swift
+//  InstaUiKit
+//
+//  Created by IPS-161 on 27/11/23.
+//
+
+import UIKit
+
+class FollowersAndFollowingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+}
