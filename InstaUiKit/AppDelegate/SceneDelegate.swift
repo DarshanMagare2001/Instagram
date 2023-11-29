baf017e42afab64c185c00208ce22680a524942b
Darshan <darshanm.itpath@gmail.com>
2023-11-29 12:12:09 +0530

    new changes

--- InstaUiKit/AppDelegate/SceneDelegate.swift
+++ InstaUiKit/AppDelegate/SceneDelegate.swift
@@ -12,7 +12,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
-    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = scene as? UIWindowScene else { return }
         window = UIWindow(windowScene: windowScene)
@@ -58,6 +57,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        PersistantStorage.shared.saveContext()
     }
     
     
