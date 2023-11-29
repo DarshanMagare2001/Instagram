0b5a2d6ccab5e7b94acac4aea94f424c4dd5a032
Darshan <darshanm.itpath@gmail.com>
2023-11-29 12:33:14 +0530

    few  new changes

---
+++ InstaUiKit/Repository/CDUsersRepository/ViewModel/CDUsers+CoreData/CDUsers+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  CDUsers+CoreDataProperties.swift
+//  
+//
+//  Created by IPS-161 on 29/11/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension CDUsers {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<CDUsers> {
+        return NSFetchRequest<CDUsers>(entityName: "CDUsers")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var name: String?
+    @NSManaged public var userName: String?
+    @NSManaged public var email: String?
+    @NSManaged public var password: String?
+
+}
