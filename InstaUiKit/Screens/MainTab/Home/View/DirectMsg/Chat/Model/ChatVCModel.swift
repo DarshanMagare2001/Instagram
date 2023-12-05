285c0f3082845ec9f4f1378943e20be09734e56f
Darshan <darshanm.itpath@gmail.com>
2023-12-05 13:00:12 +0530

    push for messagekit

--- InstaUiKit/Screens/MainTab/Home/View/DirectMsg/Chat/Model/ChatVCModel.swift
+++ InstaUiKit/Screens/MainTab/Home/View/DirectMsg/Chat/Model/ChatVCModel.swift
@@ -6,10 +6,16 @@
 //
 
 import Foundation
+import MessageKit
 
-struct Message {
-    let senderID: String
-    let receiverID: String
-    let text: String
-    let timestamp: Double
+struct Message: MessageType {
+    var sender: SenderType
+    var messageId: String
+    var sentDate: Date
+    var kind: MessageKind
+}
+
+struct Sender: SenderType {
+    var senderId: String
+    var displayName: String
 }
--- InstaUiKit/Screens/MainTab/Home/View/DirectMsg/Chat/View/ChatVC.swift
+++ InstaUiKit/Screens/MainTab/Home/View/DirectMsg/Chat/View/ChatVC.swift
@@ -1,142 +1,123 @@
-//
-//  ChatVC.swift
-//  InstaUiKit
-//
-//  Created by IPS-161 on 02/11/23.
-//
-
 import UIKit
 import Firebase
+import MessageKit
+import InputBarAccessoryView
 
-class ChatVC: UIViewController {
-    @IBOutlet weak var userImg: CircleImageView!
-    @IBOutlet weak var nameLbl: UILabel!
-    @IBOutlet weak var tableViewOutlet: UITableView!
-    @IBOutlet weak var msgTxtFld: UITextField!
-    var viewModel = ChatVCViewModel()
+class ChatVC: MessagesViewController {
+    var currentUser: SenderType?
     var receiverUser: UserModel?
-    var currentUid : String?
+    var messages: [Message] = []
+    var messagesRef: DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let receiverUser = receiverUser {
-            if let url = receiverUser.imageUrl {
-                ImageLoader.loadImage(for: URL(string: url), into: userImg, withPlaceholder: UIImage(systemName: "person.fill"))
-            }
-            nameLbl.text = receiverUser.name
-            // Get the UID of the current user
-            Data.shared.getData(key: "CurrentUserId") { (result: Result<String?, Error>) in
-                switch result {
-                case .success(let uid):
-                    if let currentUID = uid {
-                        self.currentUid = currentUID
-                        self.viewModel.startChat(senderUid: currentUID, receiverID: receiverUser.uid) { value in
-                            if value {
-                                self.tableViewOutlet.reloadData()
-                                let indexPath = IndexPath(row: self.viewModel.messages.count - 1, section: 0)
-                                self.tableViewOutlet.scrollToRow(at: indexPath, at: .bottom, animated: true)
-                            }else{
-                                self.tableViewOutlet.reloadData()
-                                let indexPath = IndexPath(row: self.viewModel.messages.count - 1, section: 0)
-                                self.tableViewOutlet.scrollToRow(at: indexPath, at: .bottom, animated: true)
-                            }
-                        }
-                    }
-                case .failure(let error):
-                    print(error)
-                }
-            }
+        guard let currentUserId = Auth.auth().currentUser?.uid else {
+            // Handle the case where the user is not logged in
+            return
         }
         
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(userImageTapped))
-        userImg.addGestureRecognizer(tapGesture)
-        userImg.isUserInteractionEnabled = true
+        currentUser = Sender(senderId: currentUserId, displayName: "Your Display Name")
         
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messageInputBar.delegate = self
+        
+        guard let receiverUserId = receiverUser?.uid else {
+            // Handle the case where the receiverUser is not set
+            return
+        }
+        
+        messagesRef = Database.database().reference().child("messages").child(chatPath(senderId: currentUserId, receiverId: receiverUserId))
+        
+        observeMessages()
     }
     
-    @objc func userImageTapped() {
-        let storyboard = UIStoryboard.MainTab
-        let destinationVC = storyboard.instantiateViewController(withIdentifier: "UsersProfileView") as! UsersProfileView
-        guard let receiverUser = receiverUser else { return }
-        destinationVC.user = receiverUser
-        destinationVC.isFollowAndMsgBtnShow = false
-        navigationController?.pushViewController(destinationVC, animated: true)
-    }
-    
-    
-    @IBAction func backBtnPressed(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
-    }
-    
-    @IBAction func sendBtnPressed(_ sender: UIButton) {
-        Data.shared.getData(key: "CurrentUserId") { (result: Result<String?, Error>) in
-            switch result {
-            case .success(let uid):
-                if let uid = uid {
-                    if let messageText = self.msgTxtFld.text, !messageText.isEmpty {
-                        if let receiverID = self.receiverUser?.uid {
-                            self.sendMessage(senderID: uid, receiverID: receiverID, text: messageText)
-                            if let fmcToken = self.receiverUser?.fcmToken {
-                                Data.shared.getData(key: "Name") {  (result: Result<String?, Error>) in
-                                    switch result {
-                                    case .success(let name):
-                                        if let name = name {
-                                            PushNotification.shared.sendPushNotification(to: fmcToken, title: name , body: messageText)
-                                        }
-                                    case.failure(let error):
-                                        print(error)
-                                    }
-                                }
-                            }
-                        }
-                        self.msgTxtFld.text = ""
-                    }
-                }
-            case .failure(let error):
-                print(error)
+    func observeMessages() {
+        messagesRef.observe(.childAdded) { snapshot in
+            guard let messageData = snapshot.value as? [String: Any],
+                  let senderId = messageData["senderId"] as? String,
+                  let displayName = messageData["displayName"] as? String,
+                  let messageId = messageData["messageId"] as? String,
+                  let sentDateString = messageData["sentDate"] as? String,
+                  let sentDate = Formatter.iso8601.date(from: sentDateString),
+                  let kindString = messageData["kind"] as? String else {
+                      return
+                  }
+            
+            let sender = Sender(senderId: senderId, displayName: displayName)
+            let messageKind: MessageKind
+            
+            switch kindString {
+            case "text":
+                let text = messageData["text"] as? String ?? ""
+                messageKind = .text(text)
+                // Add more cases for other message types if needed
+                
+            default:
+                return
             }
+            
+            let message = Message(sender: sender, messageId: messageId, sentDate: sentDate, kind: messageKind)
+            self.messages.append(message)
+            self.messagesCollectionView.reloadData()
+            self.messagesCollectionView.scrollToLastItem(animated: true)
         }
     }
     
-    
-    
-    // Function to send a message
-    func sendMessage(senderID: String, receiverID: String, text: String) {
-        let messageRef = viewModel.messagesRef?.childByAutoId()
-        let messageData: [String: Any] = [
-            "senderID": senderID,
-            "receiverID": receiverID,
-            "text": text,
-            "timestamp": ServerValue.timestamp()
-        ]
-        messageRef?.setValue(messageData) { (error, _) in
-            if let error = error {
-                print("Error sending message: \(error)")
-            }
-        }
+    func sendMessage(text: String) {
+        guard let currentUser = currentUser,
+              let receiverUserId = receiverUser?.uid else {
+                  return
+              }
+        
+        let messageRef = messagesRef.childByAutoId()
+        let message = ["senderId": currentUser.senderId,
+                       "displayName": currentUser.displayName,
+                       "messageId": messageRef.key,
+                       "sentDate": Formatter.iso8601.string(from: Date()),
+                       "kind": "text",
+                       "text": text]
+        
+        messageRef.setValue(message)
     }
     
+    func chatPath(senderId: String, receiverId: String) -> String {
+        // Create a unique path for the chat based on the sender and receiver's uids
+        return senderId < receiverId ? "\(senderId)_\(receiverId)" : "\(receiverId)_\(senderId)"
+    }
 }
 
-
-extension ChatVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.viewModel.messages.count
+extension ChatVC: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    // Implement MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate methods
+    // ...
+    
+    func currentSender() -> SenderType {
+        return currentUser!
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MsgCell", for: indexPath) as! MsgCell
-        cell.msgLbl.text = viewModel.messages[indexPath.row].text
-        if let currentUid = currentUid {
-            if viewModel.messages[indexPath.row].senderID == currentUid {
-                cell.v2.isHidden = true
-                cell.v1.isHidden = false
-            }else{
-                cell.v1.isHidden = true
-                cell.v2.isHidden = false
-            }
-        }
-        return cell
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
     }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+}
+
+extension ChatVC: InputBarAccessoryViewDelegate {
+    func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
+        sendMessage(text: text)
+        inputBar.inputTextView.text = ""
+    }
+}
+
+struct Formatter {
+    static let iso8601: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        return formatter
+    }()
 }
 
