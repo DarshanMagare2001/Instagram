884990d059760e77f61c0ecb31bc93c6984196c6
Darshan <darshanm.itpath@gmail.com>
2023-12-13 11:23:58 +0530

    new changes

--- InstaUiKit/Common/ProfilePresentedView/ProfilePresentedView.swift
+++ InstaUiKit/Common/ProfilePresentedView/ProfilePresentedView.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class ProfilePresentedView: UIViewController {
     @IBOutlet var mainView: UIView!
-    
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var userImg: CircleImageView!
+    @IBOutlet weak var userNameLbl: UILabel!
+    @IBOutlet weak var bioLbl: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         setBlurView()
