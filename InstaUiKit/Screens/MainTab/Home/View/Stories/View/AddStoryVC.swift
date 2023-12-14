fa5ae518a191b036e5367894bc23311f2bd1425c
Darshan <darshanm.itpath@gmail.com>
2023-12-14 15:44:08 +0530

    new changes

--- InstaUiKit/Screens/MainTab/Home/View/Stories/View/AddStoryVC.swift
+++ InstaUiKit/Screens/MainTab/Home/View/Stories/View/AddStoryVC.swift
@@ -4,37 +4,52 @@
 //
 //  Created by IPS-161 on 24/11/23.
 //
-
 import UIKit
 import YPImagePicker
 
 class AddStoryVC: UIViewController {
     var config = YPImagePickerConfiguration()
     var imgPicker = YPImagePicker()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         config.library.maxNumberOfItems = 4
         config.screens = [.library, .photo, .video]
         config.library.mediaType = .photoAndVideo
         imgPicker = YPImagePicker(configuration: config)
-        self.present(self.imgPicker, animated: true, completion: nil)
-        self.imgPicker.didFinishPicking { items, cancelled in
+        
+        // Display the YPImagePicker directly on the main view
+        presentImagePicker()
+    }
+    
+    func presentImagePicker() {
+        // Check if the image picker is already presented
+        if let topViewController = UIApplication.topViewController() {
+            if topViewController.presentedViewController is YPImagePicker {
+                return
+            }
+        }
+        
+        // Present the image picker on the main view
+        imgPicker.didFinishPicking { [weak self] items, cancelled in
             for item in items {
                 switch item {
                 case .photo(let photo):
                     print("Photo Selected")
-                    
                 case .video(let video):
                     print("Video Selected")
                 }
             }
-            self.imgPicker.dismiss(animated: true, completion: nil)
+            self?.imgPicker.dismiss(animated: true, completion: nil)
         }
+        
+        self.addChild(imgPicker)
+        self.view.addSubview(imgPicker.view)
+        imgPicker.didMove(toParent: self)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         navigationItem.hidesBackButton = true
-        navigationItem.title = "Add Story"
         let backButton = UIBarButtonItem(image: UIImage(named: "BackArrow"), style: .plain, target: self, action: #selector(backButtonPressed))
         backButton.tintColor = .black
         navigationItem.leftBarButtonItem = backButton
@@ -43,5 +58,21 @@ class AddStoryVC: UIViewController {
     @objc func backButtonPressed() {
         navigationController?.popViewController(animated: true)
     }
-    
+}
+
+extension UIApplication {
+    class func topViewController(_ base: UIViewController? = UIApplication.shared.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return topViewController(nav.visibleViewController)
+        }
+        if let tab = base as? UITabBarController {
+            if let selected = tab.selectedViewController {
+                return topViewController(selected)
+            }
+        }
+        if let presented = base?.presentedViewController {
+            return topViewController(presented)
+        }
+        return base
+    }
 }
