cdffcdef2cc301c66948ee7acc466288275a6dcb
Darshan <darshanm.itpath@gmail.com>
2023-12-26 14:39:17 +0530

    new chages

--- InstaUiKit/Repository/CoreDataRepository/CDUsersRepository/ViewModel/CDUserManager/CDUserManager.swift
+++ InstaUiKit/Repository/CoreDataRepository/CDUsersRepository/ViewModel/CDUserManager/CDUserManager.swift
@@ -8,9 +8,18 @@
 import Foundation
 import CoreData
 
+protocol CDUserManagerProtocol{
+    func createUser(user : CDUsersModel , completion : @escaping (Bool) -> Void)
+    func readUser() async throws -> [CDUsersModel]?
+    func deleteUser(withId id: UUID, completion: @escaping (Bool) -> Void)
+}
+
 class CDUserManager {
     static let shared = CDUserManager()
     private init(){}
+}
+
+extension CDUserManager : CDUserManagerProtocol {
     
     func createUser(user : CDUsersModel , completion : @escaping (Bool) -> Void){
         let userContext = CDUsers(context: PersistantStorage.shared.persistentContainer.viewContext)
@@ -21,7 +30,7 @@ class CDUserManager {
         PersistantStorage.shared.saveContext()
         completion(true)
     }
-  
+    
     func readUser() async throws -> [CDUsersModel]? {
         var users = [CDUsersModel]()
         do {
@@ -41,22 +50,21 @@ class CDUserManager {
     }
     
     func deleteUser(withId id: UUID, completion: @escaping (Bool) -> Void) {
-           do {
-               let fetchRequest: NSFetchRequest<CDUsers> = CDUsers.fetchRequest()
-               fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
-
-               let context = PersistantStorage.shared.persistentContainer.viewContext
-               if let userToDelete = try context.fetch(fetchRequest).first {
-                   context.delete(userToDelete)
-                   PersistantStorage.shared.saveContext()
-                   completion(true)
-               } else {
-                   completion(false)
-               }
-           } catch {
-               print(error)
-               completion(false)
-           }
-       }
-    
+        do {
+            let fetchRequest: NSFetchRequest<CDUsers> = CDUsers.fetchRequest()
+            fetchRequest.predicate = NSPredicate(format: "id == %@", id as CVarArg)
+            
+            let context = PersistantStorage.shared.persistentContainer.viewContext
+            if let userToDelete = try context.fetch(fetchRequest).first {
+                context.delete(userToDelete)
+                PersistantStorage.shared.saveContext()
+                completion(true)
+            } else {
+                completion(false)
+            }
+        } catch {
+            print(error)
+            completion(false)
+        }
+    }
 }
