c5a20547c091b6314b412fa53a49eae47affd36e
Darshan <darshanm.itpath@gmail.com>
2023-12-27 14:12:23 +0530

    new changes in SignUp VC

--- InstaUiKit/Screens/Authentication/SignUp/SignUpVCBuilder.swift
+++ InstaUiKit/Screens/Authentication/SignUp/SignUpVCBuilder.swift
@@ -5,13 +5,24 @@
 //  Created by IPS-161 on 27/12/23.
 //
 
-import Foundation
 import UIKit
 
-final class SignUpVCBuilder{
+final class SignUpVCBuilder {
     static func build() -> UIViewController {
         let storyboard = UIStoryboard.Authentication
         let signUpVC = storyboard.instantiateViewController(withIdentifier: "SignUpVC") as! SignUpVC
+        signUpVC.navigationItem.hidesBackButton = true
+        let backButton = UIBarButtonItem(image: UIImage(named: "BackArrow"), style: .plain, target: self, action: #selector(backButtonPressed))
+        backButton.tintColor = .black
+        signUpVC.navigationItem.leftBarButtonItem = backButton
         return signUpVC
     }
+    
+    @objc static func backButtonPressed(){
+        
+    }
+    
 }
+
+
+
