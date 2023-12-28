03b1203d5c6b938ca7b75b71762709e69ced5a57
Darshan <darshanm.itpath@gmail.com>
2023-12-28 15:47:53 +0530

    new chages

--- InstaUiKit/Screens/MainTab/HomeComponents/Notification/NotificationCell/NotificationCell.swift
+++ InstaUiKit/Screens/MainTab/HomeComponents/Notification/NotificationCell/NotificationCell.swift
@@ -36,45 +36,48 @@ class NotificationCell: UITableViewCell {
     
     func configureCell(currentUser:UserModel? , indexPath : Int ){
         if let cellData = currentUser , let uid = cellData.followersRequest?[indexPath] {
-            FetchUserData.shared.fetchUserDataByUid(uid:uid) { result in
-                switch result {
-                case.success(let user):
-                    if let user = user , let imgUrl = user.imageUrl , let name = user.name {
-                        self.name.text = name
-                        ImageLoader.loadImage(for: URL(string:imgUrl), into: self.userImg, withPlaceholder: UIImage(systemName: "person.fill"))
-                        
-                        self.acceptBtnBtnTapped = { [weak self] in
-                            MessageLoader.shared.showLoader(withText: "Accepting..")
-                            self?.viewModel.acceptFollowRequest(toFollowsUid: cellData.uid, whoFollowingsUid: uid){ bool in
-                                if let toFollowsUid = cellData.uid {
-                                    StoreUserData.shared.saveFollowingsToFirebaseOfUser(toFollowsUid: toFollowsUid, whoFollowingsUid: uid) { _ in
-                                        self?.viewModel.removeFollowRequest(toFollowsUid: toFollowsUid, whoFollowingsUid: uid) { bool in
-                                            if let fmcToken = user.fcmToken , let name = cellData.name {
-                                                PushNotification.shared.sendPushNotification(to: fmcToken, title: "Request Accepted" , body: "\(name) Accepted your follow request.")
+            DispatchQueue.global(qos: .background).async { [weak self] in
+                FetchUserData.shared.fetchUserDataByUid(uid:uid) { result in
+                    switch result {
+                    case.success(let user):
+                        if let user = user , let imgUrl = user.imageUrl , let name = user.name {
+                            DispatchQueue.main.async {
+                                self?.name.text = name
+                                ImageLoader.loadImage(for: URL(string:imgUrl), into: (self?.userImg)!, withPlaceholder: UIImage(systemName: "person.fill"))
+                            }
+                            self?.acceptBtnBtnTapped = { [weak self] in
+                                MessageLoader.shared.showLoader(withText: "Accepting..")
+                                self?.viewModel.acceptFollowRequest(toFollowsUid: cellData.uid, whoFollowingsUid: uid){ bool in
+                                    if let toFollowsUid = cellData.uid {
+                                        StoreUserData.shared.saveFollowingsToFirebaseOfUser(toFollowsUid: toFollowsUid, whoFollowingsUid: uid) { _ in
+                                            self?.viewModel.removeFollowRequest(toFollowsUid: toFollowsUid, whoFollowingsUid: uid) { bool in
+                                                if let fmcToken = user.fcmToken , let name = cellData.name {
+                                                    PushNotification.shared.sendPushNotification(to: fmcToken, title: "Request Accepted" , body: "\(name) Accepted your follow request.")
+                                                }
+                                                self?.fetchCurrentUseClosure?()
+                                                MessageLoader.shared.hideLoader()
                                             }
-                                            self?.fetchCurrentUseClosure?()
-                                            MessageLoader.shared.hideLoader()
                                         }
                                     }
                                 }
                             }
-                        }
-                        
-                        self.rejectBtnBtnBtnTapped = { [weak self] in
-                            MessageLoader.shared.showLoader(withText: "Rejecting..")
-                            if let toFollowsUid = cellData.uid {
-                                self?.viewModel.removeFollowRequest(toFollowsUid: toFollowsUid, whoFollowingsUid: uid) { bool in
-                                    self?.fetchCurrentUseClosure?()
-                                    MessageLoader.shared.hideLoader()
+                            
+                            self?.rejectBtnBtnBtnTapped = { [weak self] in
+                                MessageLoader.shared.showLoader(withText: "Rejecting..")
+                                if let toFollowsUid = cellData.uid {
+                                    self?.viewModel.removeFollowRequest(toFollowsUid: toFollowsUid, whoFollowingsUid: uid) { bool in
+                                        self?.fetchCurrentUseClosure?()
+                                        MessageLoader.shared.hideLoader()
+                                    }
                                 }
                             }
+                            
                         }
+                    case.failure(let error):
+                        print(error)
                     }
-                case.failure(let error):
-                    print(error)
                 }
             }
         }
     }
-    
 }
